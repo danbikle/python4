7755ac7f36aba2cb1b2954c564a9cf3eb3c2e9ce
Ron Railsmith <railsmith4@outlook.com>
2019-01-03 13:29:39 -0800

    more

--- test/system/home_indices_test.rb
+++ test/system/home_indices_test.rb
@@ -1,37 +1,37 @@
 require "application_system_test_case"
 
 class HomeIndicesTest < ApplicationSystemTestCase
-  test "visiting home/index" do
+  test "visiting top links" do
     visit '/'
-    sleep 1
+    sleep 0
     visit '/about'
-    sleep 1
+    sleep 0
     visit '/blog'
-    sleep 1
+    sleep 0
     visit '/contact'
-    sleep 1
+    sleep 0
     visit '/cclasses/class01'
-    sleep 1
+    sleep 0
     visit '/cclasses/class02'
-    sleep 1
+    sleep 0
     visit '/cclasses/class03'
-    sleep 1
+    sleep 0
     visit '/cclasses/class04'
-    sleep 1
+    sleep 0
     visit '/cclasses/class05'
-    sleep 1
-    visit '/cclasses/class06'
-    sleep 1
+    sleep 0
+    # visit '/cclasses/class06'
+    sleep 0
     visit '/cclasses/class07'
-    sleep 1
+    sleep 0
     visit '/cclasses/class08'
-    sleep 1
+    sleep 0
     visit '/cclasses/class09'
-    sleep 1
+    sleep 0
     visit '/cclasses/class10'
-    sleep 1
+    sleep 0
     visit '/'
-    sleep 3
+    sleep 0
     
     assert_selector "h1", text: "This Is About Python"
   end
